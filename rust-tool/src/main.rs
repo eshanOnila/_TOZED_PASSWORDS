7c552998947bd8ef87e2146a520fb82c3da12765
eshanOnila <eshanonilabc@gmail.com>
2025-09-25 14:29:31 +0530

    complete rust files

--- rust-tool/src/main.rs
+++ rust-tool/src/main.rs
@@ -1,3 +1,21 @@
+use std::env;
+use std::process;
+use colored::*;
+use tozed_password_rust_tool::run;
+use tozed_password_rust_tool::Config;
+
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config: Config = Config::new(&args).unwrap_or_else(|err|{
+        println!("Error: {}",err.red());
+        process::exit(1);
+    });
+    println!("IMEI: {}",config.imei);
+    println!("MAC: {}",config.mac);
+
+    if let Err(e) = run(&config) {
+        println!("Application error: {}",e);
+        process::exit(1);        
+    }
 }
